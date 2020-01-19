9fe5c9085368db27c8b0c567f1005396e50e56f0
Dalton Gorman <djgotown@gmail.com>
2020-01-18 22:31:45 -0500

    Programming Assignment 1

---
+++ src/gorman_pa01/GoodbyeWorld.java
@@ -0,0 +1,10 @@
+package gorman_pa01;
+
+public class GoodbyeWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("%s%n","Goodbye, World!");
+	}
+
+}
